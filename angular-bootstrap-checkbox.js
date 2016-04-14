3896c5c4506d687358a22240a12f56ef4e3f395c
Tommy Kubica <tommy.kubica@tu-dresden.de>
2016-04-14 14:13:27 +0200

    Add font awesome icon instead of glyphicon icon

--- angular-bootstrap-checkbox.js
+++ angular-bootstrap-checkbox.js
@@ -7,7 +7,7 @@ angular.module("ui.checkbox", []).directive("checkbox", function() {
 		restrict: "E",
 		replace: "true",
 		template: "<button type=\"button\" ng-style=\"stylebtn\" class=\"btn btn-default\" ng-class=\"{'btn-xs': size==='default', 'btn-sm': size==='large', 'btn-lg': size==='largest', 'checked': checked===true}\">" +
-			"<span ng-style=\"styleicon\" class=\"glyphicon\" ng-class=\"{'glyphicon-ok': checked===true}\"></span>" +
+			"<span ng-style=\"styleicon\" class=\"fa\" ng-class=\"{'fa-check': checked===true}\"></span>" +
 			"</button>",
 		compile: function compile(elem, attrs, transclude) {
 			if(attrs.ngClass !== undefined) {
